d5443d0a8895059567b6b3cd4d554547d7395081
Henri Laukka <t2lahe00@students.oamk.fi>
2024-03-27 18:43:12 +0200

    Started REST documentation

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MTE2MA/spectral.js?branch_slug=main&token=b1c1de8f-d5ec-4b7e-8b4e-3dd36367c3dc";
+export default { extends: ruleset };
